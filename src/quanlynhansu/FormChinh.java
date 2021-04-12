9407c207a36fece397f31f4c8857560d0d066a24
Admin <Admin@DESKTOP-G1C122B>
2021-04-12 12:14:07 +0700

    test

--- src/quanlynhansu/FormChinh.java
+++ src/quanlynhansu/FormChinh.java
@@ -16,9 +16,7 @@ public class FormChinh extends javax.swing.JFrame {
 //        meAD.setVisible(false);
 
     }
-
-    
-
+   
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
